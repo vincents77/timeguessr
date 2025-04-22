204f1c0c8106f5d947ab5ef97d209d1782887000
Vincent Seris <vincentseris@MacBook-Air-de-Vincent.local>
2025-04-23 01:20:24 +0200

    Fix: leaflet marker icon using custom import

--- src/TimeGuessrGame.jsx
+++ src/TimeGuessrGame.jsx
@@ -3,6 +3,19 @@ import { MapContainer, TileLayer, Marker, Popup, Polyline, useMap } from 'react-
 import { useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import supabase from './supabaseClient';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 function MapSelector({ onSelect }) {
   useMapEvents({ click: (e) => onSelect([e.latlng.lat, e.latlng.lng]) });
